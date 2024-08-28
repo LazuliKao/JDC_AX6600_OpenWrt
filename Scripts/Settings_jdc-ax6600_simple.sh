09005e8c5914571245252c35b06fd26ba6a213c4
LazuliKao <LazuliKao233@outlook.com>
2024-08-28 16:28:10 +0800

    sync

---
+++ Scripts/Settings_jdc-ax6600_simple.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#_______________我添加的_____________________
+# 模版  echo "CONFIG_PACKAGE_luci-app-linkease=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-app-store=y"  >> ./.config
+
+
+echo "CONFIG_PACKAGE_cpufreq=y"  >> ./.config
+echo "CONFIG_PACKAGE_cpulimit=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-accesscontrol=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-app-cpufreq=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-cpulimit=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-disks-info=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-onliner=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-openvpn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-ramfree=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-timecontrol=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-timewol=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-ttyd=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-app-verysync=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-wrtbwmon=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-zerotier=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-app-passwall=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_iptables-mod-socket=y"  >> ./.config
+echo "CONFIG_PACKAGE_iptables-mod-iprange=y"  >> ./.config
+echo "CONFIG_PACKAGE_iptables-mod-conntrack-extra=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-app-arpbind=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-arpbind-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-eqos=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-eqos-zh-cn=y"  >> ./.config
+
+
+echo "CONFIG_PACKAGE_luci-i18n-accesscontrol-zh-cn=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-i18n-cpufreq-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-cpulimit-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-disks-info-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-openvpn-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-ramfree-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-timecontrol-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-timewol-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_luci-i18n-verysync-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-wrtbwmon-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y"  >> ./.config
+echo "CONFIG_PACKAGE_ttyd=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_verysync=y"  >> ./.config
+echo "CONFIG_PACKAGE_wrtbwmon=y"  >> ./.config
+echo "CONFIG_PACKAGE_zerotier=y"  >> ./.config
+
+echo "CONFIG_PACKAGE_docker=y"  >> ./.config
+echo "CONFIG_PACKAGE_dockerd=y"  >> ./.config
+#echo "CONFIG_PACKAGE_docker-compose=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-lib-docker=y"  >> ./.config
+#echo "CONFIG_PACKAGE_luci-app-docker=y"  >> ./.config
+#echo "CONFIG_PACKAGE_dockerman=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-app-dockerman=y"  >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y"  >> ./.config
+
+#echo "CONFIG_PACKAGE_linkease=y"  >> ./.config
+#echo "CONFIG_PACKAGE_ffmpeg-remux=y"  >> ./.config
+#echo "CONFIG_PACKAGE_linkmount=y"  >> ./.config
+#echo "CONFIG_PACKAGE_luci-app-linkease=y"  >> ./.config
+
+#echo "CONFIG_PACKAGE_luci-theme-netgear=y"  >> ./.config
+
+
+
+
+#_______________我添加的_____________________
